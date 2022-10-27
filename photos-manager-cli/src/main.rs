1fe706ecae6c9c9fe0e404708f1e8dcc8279acdd
Victor Quiroz <git@victorhqc.com>
2022-10-27 17:15:03 +0200

    feat: add order command
    It doesn't do anything yet

--- photos-manager-cli/src/main.rs
+++ photos-manager-cli/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, Subcommand};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+    println!("{:?}", args);
+    match args.cmd {
+        SubCommand::Order { path } => {
+            println!("This is the path: {:?}", path);
+        }
+    }
+}
+#[derive(Parser, Debug)]
+#[clap(
+    author = "Victor Quiroz Castro",
+    version,
+    about = "Utilities for photos"
+)]
+struct Arguments {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    /// Order photos by date (YYYY-mm)
+    Order {
+        /// Path to your photos: `C:\path\to\your\photos` or `/path/to/your/photos` depending on your OS
+        path: String,
+    },
 }
