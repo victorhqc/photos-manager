f8eae9c92cc72b509ddfb0c877be30ae37016a3b
Victor Quiroz <git@victorhqc.com>
2023-04-16 18:18:16 +0200

    chore: fix syntax

--- scripts/linux/install_imagemagick.sh
+++ scripts/linux/install_imagemagick.sh
@@ -4,9 +4,9 @@ sudo apt-get update
 sudo apt-get -y install curl build-essential clang pkg-config libjpeg-turbo-progs libpng-dev
 
 curl https://imagemagick.org/archive/ImageMagick.tar.gz | tar xz
-cd ImageMagick-${MAGICK_VERSION}* \
-./configure --with-magick-plus-plus=no --with-perl=no \
-make \
-sudo make install \
-cd .. \
+cd ImageMagick-${MAGICK_VERSION}*
+./configure --with-magick-plus-plus=no --with-perl=no
+make
+sudo make install
+cd ..
 rm -r ImageMagick-${MAGICK_VERSION}*
